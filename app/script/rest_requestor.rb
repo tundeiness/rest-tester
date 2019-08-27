78173ce743e6b0f7e332cc8d7c5f07d40edf13c5
tundeiness <tundeiness@yahoo.co.uk>
2019-08-27 12:17:39 +0100

    generate Users controller and index action

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,7 +1,7 @@
 require 'rest-client'
-#url = "http://localhost:3000"
+url = "http://localhost:3000"
 
-puts "Please enter url:> "
-search = gets.chomp
+#puts "Please enter url:> "
+#search = gets.chomp
 
-puts RestClient.get(search)
+puts RestClient.get(url)
